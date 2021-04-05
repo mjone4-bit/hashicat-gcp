1480fafe4aa19420333a6ca13a8c4c8c7295e9f4
Michael Jones <mjone4@amfam.com>
2021-04-05 18:59:32 +0000

    Added VPC Module

--- main.tf
+++ main.tf
@@ -109,3 +109,16 @@ resource "null_resource" "configure-cat-app" {
     }
   }
 }
+module "network" {
+  source  = "app.terraform.io/mjone4-training/network/google"
+  version = "2.5.0"
+    project_id   = "var.project"
+    network_name = "mjone4-vpc"
+  subnets = [
+  {
+    subnet_name   = "mjone4-subnet"
+    subnet_ip     = "10.100.10.0/24"
+    subnet_region = var.region
+  }
+]
+}
